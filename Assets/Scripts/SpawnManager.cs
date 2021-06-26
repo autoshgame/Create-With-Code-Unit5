d6b6c969d3efd1b83a52b932fa90af0754dfe27c
autoshgame <66913909+autoshgame@users.noreply.github.com>
2021-06-26 16:08:13 +0700

    Complete lesson 5.1 - Clicky mouse
    - Create project and switch to 2D view
    - Create good and bad targets
    - Toss objects randomly in the air
    - Replace messy code with new methods
    - Create object list in Game Manager
    - Create a coroutine to spawn objects
    - Destroy target with click and sensor

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] protected List<GameObject> targets;
+
+    private float spawnDelay = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    IEnumerator SpawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnDelay);
+            int targetIndex = Random.Range(0, targets.Count);
+            Instantiate(targets[targetIndex], targets[targetIndex].transform.position, targets[targetIndex].transform.rotation );
+        }
+    }
+
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = this.GetComponent<Rigidbody>();
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRB.AddTorque(RandomTorque(), ForceMode.Impulse);
+        this.transform.position = RandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        outOfBound();
+    }
+    
+    public Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-4, 4), -2, 0);
+    }
+
+    public Vector3 RandomTorque()
+    {
+        return new Vector3(Random.value, Random.value, Random.value);
+    }
+
+    public Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(12, 16);
+    }
+
+
+    public void outOfBound()
+    {
+        if (this.transform.position.y <= -3f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+ 
+
+    public void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+
+
+    
+
+
+
+
+
+}
